06f88cbb4b871f11bb4d87405a2037a0f8cda881
Onur <onur.sazak@hotmail.com.tr>
2020-07-18 18:59:08 +0300

    vuedocs-modules

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<img
+			alt="Vue logo"
+			src="./assets/logo.png"
+		>
+		<label for="phoneNumber">Phone number</label>
+		<input
+			id="phoneNumber"
+			type="tel"
+			placeholder="Phone number"
+			v-model="getNumber"
+		>
+		{{ getNumber }}
+		<!-- <child-component>Content..</child-component> -->
+		<button type="button" @click="getTheValue()">get the value</button>
+
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+	import ChildComponent from "./components/ChildComponent.vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		components: {
+			childComponent: ChildComponent
+		},
+		methods: {
+			changePhoneNumber: function(event) {
+				console.log(event.target.value);
+				// this.$store.commit('UpdateNumber',event.target.value);
+			},
+			getTheValue: function() {
+				console.log(this.$store.getters.doubleCount);
+			}
+		},
+		computed: {
+			getNumber: {
+				get() {
+					return this.$store.state.phoneNumber;
+				},
+				set(value) {
+					this.$store.commit("UpdateNumber", value);
+				}
+			}
+		}
+	};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,66 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex';
 
 Vue.config.productionTip = false
 
+Vue.use(Vuex);
+
+	const moduleA = {
+		namespaced: false,
+		state: () => ({
+			count: 1,
+			name: "moduleA"
+		}),
+		mutations: {
+			increment(state) {
+				state.count++;
+			}
+		},
+		actions: {},
+		getters: {
+			doubleCount(state, getters, rootState) {
+				console.log("getter",getters['b/getName']);	
+				return state.count * 2;
+			},
+		}
+	};
+
+	const moduleB = {
+		namespaced: true,
+		state: () => ({
+			count: 2
+		}),
+		mutations: {},
+		actions: {},
+		getters: {
+			getName: (state) => {
+				return "From ModuleB";
+			},
+			doubleCount2: (state) => {
+				return "in moduleB";
+			}
+		}
+	};
+
+	const store = new Vuex.Store({
+		modules: {
+			a: moduleA,
+			b: moduleB
+		},
+		state: {
+			phoneNumber: 555
+		},
+		mutations: {
+			UpdateNumber(state,payload) {
+				state.phoneNumber = payload;
+			}
+		},
+		actions: {},
+		getters: {}
+	});
+
 new Vue({
   render: h => h(App),
+  store
 }).$mount('#app')
